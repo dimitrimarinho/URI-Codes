c9099bbaf58f186cb0b2423b82d28473969ec01e
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-12-16 17:43:22 -0300

    URI Accepted!

---
+++ Stack Codes/1110-cartasfora.cpp
@@ -0,0 +1,43 @@
+// Name: (1062) - Trilhos
+// Link: https://www.urionlinejudge.com.br/judge/pt/problems/view/1062
+
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+int main()
+{
+    int cards;
+    while(cin >> cards, cards != 0)
+    {
+        queue<int> last;
+        vector<int> discarded;
+        for(int i = 1; i <= cards; i++)
+        {
+            last.push(i);
+        }
+        while(!last.empty())
+        {
+            discarded.push_back(last.front());
+            last.pop();
+            int aux = last.front();
+            last.pop();
+            last.push(aux);
+        }
+        cout << "Discarded cards: ";
+        int cont = 1;
+        for(vector<int>::iterator it = discarded.begin(); it != discarded.end()-1; it++)
+        {
+            if(cont < cards-1)
+            {
+                cout << (*it) << ", ";
+                cont++;
+            }
+            else
+                cout << (*it);
+        }
+        cout << endl;
+        cout << "Remaining card: " << discarded[cards-1] << endl;
+
+    }
+}
