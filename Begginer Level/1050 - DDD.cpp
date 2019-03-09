a2030f340b7f3966b0fbd2a80f6c8e1af275214b
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2019-03-09 13:47:56 -0300

    URI Accepted !

---
+++ Begginer Level/1050 - DDD.cpp
@@ -0,0 +1,31 @@
+// Name of the problem: DDD
+// Link: https://www.urionlinejudge.com.br/judge/pt/problems/view/1050
+
+#include<iostream>
+using namespace std;
+int main(){
+
+	int codigoDDD;
+	cin >> codigoDDD;
+	if(codigoDDD == 61)
+		cout << "Brasilia\n";
+	else if(codigoDDD == 71)
+		cout << "Salvador\n";
+	else if(codigoDDD == 11)
+		cout << "Sao Paulo\n";
+	else if(codigoDDD == 21)
+		cout << "Rio de Janeiro\n";
+	else if(codigoDDD == 61)
+		cout << "Brasilia\n";
+	else if(codigoDDD == 32)
+		cout << "Juiz de Fora\n";
+	else if(codigoDDD == 19)
+		cout << "Campinas\n";
+	else if(codigoDDD == 27)
+		cout << "Vitoria\n";
+	else if(codigoDDD == 31)
+		cout << "Belo Horizonte\n";
+	else 
+		cout << "DDD nao cadastrado\n";
+
+}
