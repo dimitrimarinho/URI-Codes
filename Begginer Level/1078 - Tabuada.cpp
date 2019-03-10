8ae564e275a01d33b28354d0098bee1e989e4b27
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2019-03-10 12:14:14 -0300

    URI Accepted !

---
+++ Begginer Level/1078 - Tabuada.cpp
@@ -0,0 +1,13 @@
+// Name of the problem: Tabuada
+// Link: https://www.urionlinejudge.com.br/judge/pt/problems/view/1078
+
+#include<iostream>
+using namespace std;
+int main(){
+
+	int N;
+	cin >> N;
+	for (int i = 1; i <= 10; i++)
+		cout << i << " x " << N << " = " << i*N << endl;
+
+}
