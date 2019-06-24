61b71264d1bcae56a57582a559d7c81ee8a184ff
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2019-06-24 09:40:37 -0300

    URI Accepted !

--- Begginer Level/Python Codes/1097 - Sequencia IJ 3.py
+++ Begginer Level/Python Codes/1097 - Sequencia IJ 3.py
@@ -6,21 +6,21 @@ soma = 6
 
 for sequencia in range(0, 3):
     print('I=' + str(i) + ' J=' + str(i+soma))
-    soma -= 1 
+    soma -= 1
 
 i = 3
 soma = 6
 
 for sequencia in range(0, 3):
     print('I=' + str(i) + ' J=' + str(i+soma))
-    soma -= 1 
+    soma -= 1
 
 i = 5
 soma = 6
 
 for sequencia in range(0, 3):
     print('I=' + str(i) + ' J=' + str(i+soma))
-    soma -= 1 
+    soma -= 1
 
 i = 7
 soma = 6
@@ -34,4 +34,4 @@ soma = 6
 
 for sequencia in range(0, 3):
     print('I=' + str(i) + ' J=' + str(i+soma))
-    soma -= 1 
+    soma -= 1
