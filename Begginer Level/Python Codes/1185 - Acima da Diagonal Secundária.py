d66cf511ddb4720376ba049fc365d8d50b5fd078
dimitrimarinho <dimitri.santana.marinho@gmail.com>
2021-12-18 15:39:53 -0300

    Submission Accepted on the Beecrowd !

---
+++ Begginer Level/Python Codes/1185 - Acima da Diagonal Secundária.py
@@ -0,0 +1,19 @@
+# Name of the problem: Acima da Diagonal Secundária
+# Link: https://www.beecrowd.com.br/judge/pt/problems/view/1185
+
+word = input()
+sum = average = n = 0
+sub = 11
+for line in range(12):
+    for column in range(12):
+        number = float(input())
+        if(column < sub):
+            sum += number
+            n += 1
+    sub -= 1
+
+average = sum/n
+if(word == 'S'):
+    print('{:.1f}'.format(sum))
+elif(word == "M"):
+    print('{:.1f}'.format(average))
