1b5a3b86a5daea618d53d089c3a4c905a41c332c
dimitrimarinho <dimitri.santana.marinho@gmail.com>
2021-12-23 23:20:49 -0300

    Submission Accepted on the Beecrowd !

---
+++ Begginer Level/Python Codes/1564 - Vai ter Copa ?.py
@@ -0,0 +1,14 @@
+# Name of the problem: Vai ter Copa ?
+# Link: https://www.beecrowd.com.br/judge/pt/problems/view/1564
+
+while True:
+  try:
+    N = int(input())
+    if(N > 0):
+        print("vai ter duas!")
+    else:
+        print("vai ter copa!")
+  except EOFError:
+    break
+
+# Submission Accepted on the Beecrowd ! 
