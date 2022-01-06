92b7e5684b5071f1d78847f78bb2577b89ffe3ca
dimitrimarinho <dimitri.santana.marinho@gmail.com>
2022-01-06 00:02:48 -0300

    # Submission Accepted on the Beecrowd !

---
+++ Begginer/SQL Codes/2602 - Select Básico.sql
@@ -0,0 +1,8 @@
+-- Name of the problem: Select Básico
+-- Link: https://www.beecrowd.com.br/judge/pt/problems/view/2602
+
+SELECT c.name 
+FROM customers c
+WHERE c.state = 'RS';
+
+-- Submission Accepted on the Beecrowd ! 
