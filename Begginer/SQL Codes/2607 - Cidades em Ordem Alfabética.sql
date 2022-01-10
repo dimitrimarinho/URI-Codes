30530493cc5770fb1da91be4c998dcdc59e4378d
dimitrimarinho <dimitri.santana.marinho@gmail.com>
2022-01-10 20:20:41 -0300

    Submission Accepted on the Beecrowd !

---
+++ Begginer/SQL Codes/2607 - Cidades em Ordem Alfabética.sql
@@ -0,0 +1,8 @@
+-- Name of the problem: Cidades em Orgem Alfabética
+-- Link: https://www.beecrowd.com.br/judge/pt/problems/view/2607
+
+SELECT p.city
+FROM providers p
+ORDER BY ASC
+
+-- Submission Accepted on the Beecrowd ! 
