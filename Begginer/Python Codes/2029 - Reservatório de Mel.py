52b561c01ccc671285d9777a4ebd94d901f5f48b
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2023-04-10 22:07:01 -0300

    Submission Accepted on the Beecrowd !

--- Begginer/Python Codes/2029 - Reservatório de Mel.py
+++ Begginer/Python Codes/2029 - Reservatório de Mel.py
@@ -2,14 +2,12 @@
 # Link: https://www.beecrowd.com.br/judge/pt/problems/view/2029
 
 while True:
-
     try:
-
         V, D = float(input()), float(input())
         AREA = (3.14 * (D/2)**2)
         ALTURA = V / AREA
         print(f'ALTURA = {ALTURA:.2f}')
         print(f'AREA = {AREA:.2f}')
-        
+
     except EOFError:
         break
