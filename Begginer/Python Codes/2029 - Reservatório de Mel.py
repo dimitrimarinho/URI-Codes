744d339d315288ab4f53d062af35d38188d0a760
DIMITRI SANTANA MARINHO <DIMITRIM@suzano.com.br>
2023-04-10 21:59:11 -0300

    Submission Accepted on the Beecrowd !

---
+++ Begginer/Python Codes/2029 - Reservatório de Mel.py
@@ -0,0 +1,11 @@
+# Name of the problem: Reservatório de Mel
+# Link: https://www.beecrowd.com.br/judge/pt/problems/view/2029
+
+while True: 
+    try:
+        V, D = float(input()), float(input())
+        AREA = (3.14 * (D/2)**2)
+        ALTURA = V / AREA
+        print(f'ALTURA = {ALTURA:.2f}')
+        print(f'AREA = {AREA:.2f}')
+    except EOFError: break
